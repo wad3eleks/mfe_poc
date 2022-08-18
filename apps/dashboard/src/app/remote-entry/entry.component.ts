7eb22d85ae0332895c189db7564007240f5ea13c
Roman Maliarchuk <rmaliarchuk@mynexuscare.com>
2022-08-18 20:56:50 +0300

    first commit

---
+++ apps/dashboard/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfe-poc-dashboard-entry',
+  template: `<mfe-poc-nx-welcome></mfe-poc-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ apps/shell/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { loadRemoteModule } from '@nrwl/angular/mf';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'login',
+          loadChildren: () =>
+            loadRemoteModule('login', './Module').then(
+              (m) => m.RemoteEntryModule
+            ),
+        },
+        {
+          path: '',
+          component: NxWelcomeComponent,
+        },
+        {
+          path: 'dashboard',
+          loadChildren: () =>
+            loadRemoteModule('dashboard', './Module').then(
+              (m) => m.RemoteEntryModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
